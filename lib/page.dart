5b0528c4f25ff129e4d6ca5571bda0ec2edb965e
asalazar052 <asalazar052@icloud.com>
2022-07-23 22:46:45 -0400

    Update page.dart

--- lib/page.dart
+++ lib/page.dart
@@ -8,21 +8,11 @@ class PageTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      navigationBar: CupertinoNavigationBar(
+      navigationBar: const CupertinoNavigationBar(
         middle: Image(
           image: AssetImage('assets/mountie_icon.png'),
         ),
         padding: EdgeInsetsDirectional.all(2),
-        trailing: CupertinoButton(
-          onPressed: () => print('test'),
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(10),
-          child: const Icon(
-            //.bars or .line_horizontal_3
-            CupertinoIcons.line_horizontal_3,
-            color: CupertinoColors.white,
-          ),
-        ),
       ),
       child: Center(child: Text(pageText)),
     );
