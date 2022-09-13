4929a26453e1ad9445cf1060cc250cc97e29f248
asalazar052 <asalazar052@icloud.com>
2022-09-12 23:57:30 -0500

    Home Page Features
    Gave home page a Box for description and section for upcoming events

--- lib/screens/event_pages/home.dart
+++ lib/screens/event_pages/home.dart
@@ -4,12 +4,42 @@ class HomeEventPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      //navigationBar: CustomNavBar(),
-      child: Stack(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: Text('Home'),
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+              margin: const EdgeInsets.fromLTRB(50, 0, 50, 10),
+              width: 350,
+              height: 350,
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 0, 47, 255),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(20),
+                ),
+              ),
+              child: const Text(
+                "Welcome to Mountie Mobile 2!",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
           ),
+          Container(
+            margin: const EdgeInsets.fromLTRB(0, 20, 0, 250),
+            child: const Text(
+              "Upcoming Events:",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          )
         ],
       ),
     );
