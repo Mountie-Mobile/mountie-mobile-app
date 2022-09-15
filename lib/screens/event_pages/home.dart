5816ede4ce101be98f26e360d4de99df464d4fdb
asalazar052 <asalazar052@icloud.com>
2022-09-14 21:56:39 -0500

    Home screen
    Fixed text box and added shadow behind box

--- lib/screens/event_pages/home.dart
+++ lib/screens/event_pages/home.dart
@@ -12,26 +12,33 @@ class HomeEventPage extends StatelessWidget {
               padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
               margin: const EdgeInsets.fromLTRB(50, 0, 50, 10),
               width: 350,
-              height: 350,
+              height: 190,
               decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(135, 0, 0, 0),
+                    offset: Offset(5.0, 5.0),
+                    blurRadius: 5.0,
+                    spreadRadius: 2.0,
+                  ),
+                ],
                 color: Color.fromARGB(255, 0, 47, 255),
                 borderRadius: BorderRadius.all(
                   Radius.circular(20),
                 ),
               ),
               child: const Text(
-                "Welcome to Mountie Mobile 2!",
+                "Welcome to the new and improved Mountie Mobile 2! On the bottom, there is a navigator to the various categories of events from our school. If you click the 3 lines button, you can navigate to other various screens within the app.",
                 style: TextStyle(
                   color: Color.fromARGB(255, 255, 255, 255),
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
+                  fontSize: 17,
                 ),
                 textAlign: TextAlign.center,
               ),
             ),
           ),
           Container(
-            margin: const EdgeInsets.fromLTRB(0, 20, 0, 250),
+            margin: const EdgeInsets.fromLTRB(0, 20, 0, 425),
             child: const Text(
               "Upcoming Events:",
               style: TextStyle(
