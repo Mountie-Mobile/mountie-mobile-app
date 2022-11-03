8e67717af2f63cec4a386821d58e9facf08921fa
Matthew-Cool <57239986+Matthew-Cool@users.noreply.github.com>
2022-11-02 21:06:40 -0500

    changed import to fix rename issue

--- lib/page_scaffold.dart
+++ lib/page_scaffold.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-import 'screens/events.dart';
+import 'screens/eventTabs.dart';
 import 'screens/search.dart';
 import 'screens/calendar.dart';
 import 'screens/settings.dart';
